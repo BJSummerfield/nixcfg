0a4f6f31d06c81bce5136ceb9bf7b6becedab47b
BJSummerfield <brianjsummerfield@gmail.com>
2025-03-09 14:59:48 -0500

    update spacefunk

--- home/waktu/spacefunk.nix
+++ home/waktu/spacefunk.nix
@@ -1,41 +1,26 @@
-{ pkgs, config, inputs, ... }: {
+{ pkgs, ... }: {
   imports = [
     ./home.nix
     ../common
-    ../features/cli
-    ../features/desktop
+    ../features
   ];
 
   features = {
     cli = {
-      fish.enable = true;
       helix.enable = true;
-      git.enable = true;
-      ssh-1password.enable = true;
-      encoding.enable = true;
+      encode_queue.enable = true;
     };
 
     desktop = {
       hyprland.enable = true;
-      fonts.enable = true;
+      keybase.enable = true;
+      _1password.enable = true;
     };
   };
 
-  services = {
-    keybase.enable = true;
-    kbfs.enable = true;
-  };
 
   home.packages = with pkgs; [
     firefox
     steam
-    _1password-gui
-    _1password
-    keybase-gui
-    keybase
   ];
-
-  home.sessionVariables = {
-    TERMINAL = "ghostty";
-  };
 }
