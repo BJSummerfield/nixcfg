6fa16a4f310b2f4c5265141a39f5adb3a9239b38
BJSummerfield <brianjsummerfield@gmail.com>
2025-03-10 17:55:25 -0500

    revert battery to shell script

--- home/features/desktop/battery.nix
+++ home/features/desktop/battery.nix
@@ -1,56 +1,52 @@
 { config, lib, pkgs, ... }:
-with lib;
-let
+with lib; let
   cfg = config.features.desktop.battery;
-
-  batteryApp = pkgs.writeShellApplication {
-    name = "battery-notify";
-    runtimeInputs = [ pkgs.libnotify ];
-    text = ''
-      #!${pkgs.stdenv.shell}
-      
-      get_battery() {
-        cat /sys/class/power_supply/BAT0/capacity
-      }
-      while true; do
-        battery=$(get_battery)
-        if [ "$battery" -gt 50 ]; then
-          sleep 600   # 10 minutes when battery > 50%
-        elif [ "$battery" -gt 20 ]; then
-          sleep 600   # 10 minutes when battery is between 21% and 50%
-        elif [ "$battery" -gt 10 ]; then
-          notify-send -t 8000 -u normal "Battery Warning" "Battery low: $battery%"
-          sleep 120   # 2 minutes when battery is between 11% and 20%
-        else
-          notify-send -t 15000 -u critical "Battery Critical" "Battery critically low: $battery%"
-          sleep 60    # 1 minute when battery is 10% or lower
-        fi
-      done
-    '';
-  };
 in
 {
   options.features.desktop.battery.enable =
     mkEnableOption "Get battery status through notifications";
-
   config = mkIf cfg.enable {
-
-    systemd.user.services.batteryNotify = {
-      Unit = {
-        Description = "Battery Charge Notifications";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
+    systemd.user.services.batteryNotify =
+      let
+        batteryScript = pkgs.writeShellScript "battery.sh" ''
+          get_battery() {
+              cat /sys/class/power_supply/BAT0/capacity
+          }
+          while true; do
+            battery=$(get_battery)
+            if [ "$battery" -gt 50 ]; then
+                sleep 600   # 10 minutes when battery > 50%
+            elif [ "$battery" -gt 20 ]; then
+                notify-send -t 5000 -u normal "Battery Status" "Battery status: $battery%"
+                sleep 600   # 10 minutes when battery is between 21% and 50%
+            elif [ "$battery" -gt 10 ]; then
+                notify-send -t 8000 -u normal "Battery Warning" "Battery low: $battery%"
+                sleep 120   # 2 minutes when battery is between 11% and 20%
+            else
+                notify-send -t 15000 -u critical "Battery Critical" "Battery critically low: $battery%"
+                sleep 60    # 1 minute when battery is 10% or lower
+            fi
+          done
+        '';
+      in
+      {
+        Unit = {
+          Description = "Battery Charge Notifications";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${batteryScript}";
+          Restart = "always";
+          Environment = "PATH=${pkgs.libnotify}/bin:/run/current-system/sw/bin:/usr/bin:/bin";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
       };
-      Service = {
-        Type = "simple";
-        ExecStart = "${batteryApp}/bin/battery-notify";
-        Restart = "always";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-    };
-
-    home.packages = with pkgs; [ libnotify ];
+    home.packages = with pkgs; [
+      libnotify
+    ];
   };
 }
