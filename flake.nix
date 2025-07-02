71c31718ef4b645527e6d70a26540ed888584d7f
BJSummerfield <brianjsummerfield@gmail.com>
2025-07-02 16:42:16 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -4,37 +4,32 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    catppuccin.url = "github:catppuccin/nix";
-    stylix.url = "github:danth/stylix";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    _1password-shell-plugins.url = "github:1Password/shell-plugins";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { nixpkgs, ... }@inputs:
     let
-      inherit (self) outputs;
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
+      supportedSystems = [
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      packages =
-        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-      overlays = import ./overlays { inherit inputs; };
+      # packages =
+      #   forEachSupportedSystem ();
       nixosConfigurations = {
         t495 = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/t495 ];
-        };
-        redtruck = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/redtruck ];
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [ ./hosts/t495/configuration.nix ];
         };
       };
     };
