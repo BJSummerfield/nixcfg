9c9e3502a092266d74f4ec39b0ea5ac55f524fac
BJSummerfield <brianjsummerfield@gmail.com>
2025-07-03 23:43:27 -0500

    fix enabled

--- hosts/t495/default.nix
+++ hosts/t495/default.nix
@@ -12,7 +12,7 @@
         hostName = "t495";
         bootPartitionUuid = "5cccbb79-6ae4-4a43-add1-9b5fa0a03e18";
         ssh = {
-          enabled = true;
+          enable = true;
           authorizedKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtTarFZkhNoHtu39C6eCRaS84jb6SPoY92gn64Q2D3O";
         };
       };
