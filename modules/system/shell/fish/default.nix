bc61998617cbf3c9dc447d8b0f39709a2064601f
BJSummerfield <brianjsummerfield@gmail.com>
2025-07-03 19:00:15 -0500

    update fish

--- modules/system/shell/fish/default.nix
+++ modules/system/shell/fish/default.nix
@@ -1,6 +1,7 @@
 { lib, config, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
+  inherit (config.mine) user;
   cfg = config.mine.system.shell.fish;
   cli-tools = config.mine.cli-tools;
 in
@@ -10,21 +11,25 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set fish_greeting # Disable greeting
-        set EDITOR hx
-        set -x NIX_PATH nixpkgs=channel:nixos-unstable
-      '' + (lib.optionalString cli-tools.direnv.enable ''
-        direnv hook fish | source
-      '');
-      loginShellInit = ''
-        set fish_greeting # Disable greeting
-      '';
-      shellAliases = {
-        ls = mkIf cli-tools.eza.enable "eza";
-        lg = mkIf cli-tools.lazygit.enable "lazygit";
+
+    # system level fish shell for auto-complete
+    programs.fish.enable = true;
+
+    home-manager.users.${user.name} = {
+      programs.fish = {
+        enable = true;
+        interactiveShellInit = ''
+          set fish_greeting # Disable greeting
+          set EDITOR hx
+          set -x NIX_PATH nixpkgs=channel:nixos-unstable
+        '';
+        loginShellInit = ''
+          set fish_greeting # Disable greeting
+        '';
+        shellAliases = {
+          ls = mkIf cli-tools.eza.enable "eza";
+          lg = mkIf cli-tools.lazygit.enable "lazygit";
+        };
       };
     };
   };
