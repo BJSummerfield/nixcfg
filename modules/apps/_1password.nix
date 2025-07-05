79b94eab85bb283b612490001654cba00aeb8b38
BJSummerfield <brianjsummerfield@gmail.com>
2025-07-05 01:39:01 -0500

    update 1password

--- modules/apps/_1password.nix
+++ modules/apps/_1password.nix
@@ -3,11 +3,10 @@
 let
   inherit (lib) mkEnableOption mkIf;
   inherit (config.mine) user;
-  # _1passwordShellModules = inputs._1password-shell-plugins.hmModules.default;
+  _1passwordShellModules = inputs._1password-shell-plugins.hmModules.default;
   cfg = config.mine.apps._1password;
 in
 {
-  # imports = [ _1passwordShellModules ];
   options.mine.apps._1password.enable = mkEnableOption "Enable 1password config";
 
   config = mkIf cfg.enable {
@@ -20,36 +19,34 @@ in
       # polkitPolicyOwners = [ user.name ];
     };
 
-    # home-manager.users.${user.name} = {
-    # programs._1password-shell-plugins = {
-    # enable 1Password shell plugins for bash, zsh, and fish shell
-    # enable = true;
-    # the specified packages as well as 1Password CLI will be
-    # automatically installed and configured to use shell plugins
-    # plugins = with pkgs; [ gh ];
+    home-manager.users.${user.name} = {
+      imports = [ _1passwordShellModules ];
+
+      programs._1password-shell-plugins = {
+        enable = true;
+        plugins = with pkgs; [ gh ];
+      };
+
+      # TODO break this out
+      programs.git = {
+        extraConfig = {
+          user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2G3biYuL3iFvhAXYNuVzvRpAQMmFFLek3KFZV4PfDu";
+          gpg = {
+            ssh.program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+            format = "ssh";
+          };
+          commit.gpgSign = true;
+        };
+      };
+
+      # TODO break this out
+      programs.ssh = {
+        enable = true;
+        extraConfig = ''
+          Host *
+              IdentityAgent ~/.1password/agent.sock              
+        '';
+      };
+    };
   };
-
-  # TODO break this out
-  # programs.git = {
-  #   extraConfig =
-  #     {
-  #       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2G3biYuL3iFvhAXYNuVzvRpAQMmFFLek3KFZV4PfDu";
-  #       gpg = {
-  #         ssh.program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-  #         format = "ssh";
-  #       };
-  #       commit.gpgSign = true;
-  #     };
-  # };
-
-  # TODO break this out
-  # programs.ssh = {
-  #   enable = true;
-  #   extraConfig = ''
-  #     Host *
-  #         IdentityAgent ~/.1password/agent.sock              
-  #   '';
-  # };
-  # };
-  # };
 }
--- modules/system/allowUnfree.nix
+++ modules/system/allowUnfree.nix
@@ -1,15 +1,19 @@
 { lib, config, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
-  cfg = config.mine.system.nix.unfree;
+  inherit (config.mine) user;
+  cfg = config.mine.system.allowUnfree;
 in
 {
-  options.mine.system.nix.unfree = {
+  options.mine.system.allowUnfree = {
     enable = mkEnableOption "Enable Unfree packages";
   };
 
   config = mkIf cfg.enable {
     nixpkgs.config.allowUnfree = true;
-  };
 
+    home-manager.users.${user.name} = {
+      nixpkgs.config.allowUnfree = true;
+    };
+  };
 }
