c49e9fe05d3c2dfd7411ef9331e8a396a667fc18
BJSummerfield <brianjsummerfield@gmail.com>
2025-09-20 16:45:42 -0500

    add vm config

---
+++ hosts/vm-mac/default.nix
@@ -0,0 +1,61 @@
+{ ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../modules
+    ];
+
+  config = {
+    mine = {
+      system = {
+        hostName = "vm-mac";
+        bootPartitionUuid = "5cccbb79-6ae4-4a43-add1-9b5fa0a03e18";
+        fonts.enable = true;
+        openssh.enable = true;
+      };
+      desktop = {
+        fuzzel.enable = true;
+        hypridle.enable = true;
+        hyprlock.enable = true;
+        mako.enable = true;
+        niri.enable = true;
+        polkit-gnome.enable = true;
+        swaybg.enable = true;
+        theme = {
+          catppuccin.enable = true;
+          stylix.enable = true;
+        };
+      };
+      apps = {
+        _1password = {
+          enable = true;
+          sshAgent = true;
+          gitSigning = true;
+          ghPlugin = true;
+        };
+        alacritty.enable = true;
+        firefox.enable = true;
+        keybase.enable = true;
+      };
+      cli-tools = {
+        direnv.enable = true;
+        eza.enable = true;
+        gh.enable = true;
+        git.enable = true;
+        helix = {
+          enable = true;
+          lsp = {
+            markdown.enable = true;
+            nix.enable = true;
+            rust.enable = true;
+          };
+        };
+        lazygit.enable = true;
+        starship.enable = true;
+        tailscale.enable = true;
+        zoxide.enable = true;
+      };
+    };
+  };
+}
