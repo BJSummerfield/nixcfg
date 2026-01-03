9ec9b520a3af5ebf9f10d32678c90da85c08938e
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-03 12:12:50 -0600

    change lazygit to shell alias

--- modules/lazygit/lazygit.nix
+++ modules/lazygit/lazygit.nix
@@ -1,12 +1,12 @@
 { lib, config, ... }:
 {
   options.mine.user.lazygit.enable = lib.mkEnableOption "User Lazygit";
-
   config = lib.mkIf config.mine.user.lazygit.enable {
     programs.lazygit = {
       enable = true;
       settings.gui.language = "en";
     };
-    programs.fish.shellAliases.lg = "lazygit";
+    home.shellAliases.lg = "lazygit";
   };
 }
+
