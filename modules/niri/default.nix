8a470f55937e8d85b07bc98e2bca4df113f0320f
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-04 15:49:58 -0600

    update default nix

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -1,11 +1,8 @@
 { config, lib, ... }:
 {
-  options.mine.system.niri = {
-    enable = lib.mkEnableOption "Enable niri config";
-
-    config = lib.mkIf config.mine.system.niri.enable {
-      programs.niri.enable = true;
-      home-manager.sharedModules = [ ./home.nix ];
-    };
+  options.mine.system.niri.enable = lib.mkEnableOption "Enable niri config";
+  config = lib.mkIf config.mine.system.niri.enable {
+    programs.niri.enable = true;
+    home-manager.sharedModules = [ ./home.nix ];
   };
 }
