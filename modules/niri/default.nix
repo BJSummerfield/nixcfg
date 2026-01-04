7de370cbd5f42d9f521cc5ddd379df627251f9ce
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-03 21:54:53 -0600

    add niri

---
+++ modules/niri/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, ... }:
+let
+  cfg = config.mine.desktop.niri;
+in
+{
+  options.mine.system.niri = {
+    enable = lib.mkEnableOption "Enable niri config";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.niri.enable = true;
+    # mine.desktop.xwayland-satellite.enable = true;
+    # mine.apps._1password.silentStartOnGraphical = true;
+    home-manager.sharedModules = [ ./home.nix ];
+  };
+}
