644b31f9bc0cddb0e47f864a1ac67478705937ec
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-04 13:23:29 -0600

    add monitor and extra window config rules

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -5,8 +5,34 @@ in
 {
   options.mine.system.niri = {
     enable = lib.mkEnableOption "Enable niri config";
-  };
 
+    outputs = lib.mkOption {
+      description = "Attrset of outputs, where key is the output name (e.g. DP-1)";
+      default = { };
+      type = lib.types.attrsOf (lib.types.submodule {
+        options = {
+          mode = lib.mkOption {
+            type = lib.types.str;
+            description = "Resolution and refresh rate (e.g. 3440x1440@175)";
+          };
+          variableRefreshRate = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+          };
+          scale = lib.mkOption {
+            type = lib.types.float;
+            default = 1.0;
+          };
+        };
+      });
+    };
+
+    extraWindowRules = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "List of KDL window-rule blocks to append to config";
+    };
+  };
   config = lib.mkIf cfg.enable {
     programs.niri.enable = true;
     # mine.desktop.xwayland-satellite.enable = true;
