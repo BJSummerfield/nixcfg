9d1ee531a9c91d1eab49ec54816189fc8ae94ab5
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-04 22:35:30 -0600

    update default

--- modules/encode_queue/default.nix
+++ modules/encode_queue/default.nix
@@ -1,18 +1,5 @@
-{ pkgs, lib, config, ... }:
-let
-  inherit (lib) mkEnableOption mkOption types mkIf;
-  cfg = config.mine.user.encode_queue;
-in
+{ ... }:
 {
-  options.mine.user.encode_queue = {
-    enable = mkEnableOption "Encode Queue Tool";
-    package = mkOption {
-      type = types.package;
-      default = pkgs.callPackage ./package.nix { };
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-  };
+  home-manager.sharedModules = [ ./home.nix ];
 }
+
