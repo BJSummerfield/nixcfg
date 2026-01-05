ed0e823a661ecd92015f5a9b7a783e3d0f190bdc
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-04 22:33:57 -0600

    add bicep home

--- modules/bicep-langserver/default.nix
+++ modules/bicep-langserver/default.nix
@@ -1,18 +1,4 @@
-{ pkgs, lib, config, ... }:
-let
-  inherit (lib) mkEnableOption mkOption types mkIf;
-  cfg = config.mine.user.bicep-langserver;
-in
+{ ... }:
 {
-  options.mine.user.bicep-langserver = {
-    enable = mkEnableOption "Install Bicep language server";
-    package = mkOption {
-      type = types.package;
-      default = pkgs.callPackage ./package.nix { };
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-  };
+  home-manager.sharedModules = [ ./home.nix ];
 }
