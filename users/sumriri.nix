968ff2fc77fcf31562f847d0efcbd24ca1ea1ebe
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-09 17:04:35 -0600

    update hash an auto load niri

--- users/sumriri.nix
+++ users/sumriri.nix
@@ -2,9 +2,16 @@
 {
   config.mine.users.sumriri = {
     description = "Ryker";
-    isSuperUser = true;
-
-    initialHashedPassword = "$y$j9T$IoChbWGYRh.rKfmm0G86X0$bYgsWqDRkvX.EBzJTX.Z0RsTlwspADpvEF3QErNyCMC";
-
+    initialHashedPassword = "$6$fuU2Mo77wD.TUQJB$0u66e9w/vb66UKCFzc8YJVOn60Cznn7sGx9kxBXmuYzpVe1HVfZwPxtOSVdoKX925kxtFrgEJyyi6ZfbuIl6U1";
+    home-modules = [
+      {
+        programs.fish.loginShellInit = ''
+          if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+            exec niri-session -l
+          end
+        '';
+      }
+    ];
   };
+
 }
