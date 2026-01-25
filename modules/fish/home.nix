c5c85b3b025646457edc909165e14bf193ae24b6
BJSummerfield <brianjsummerfield@gmail.com>
2026-01-25 11:11:50 -0600

    add catppuccin theme file to fish

--- modules/fish/home.nix
+++ modules/fish/home.nix
@@ -3,17 +3,19 @@
   options.mine.user.fish.enable = lib.mkEnableOption "User Fish Config";
 
   config = lib.mkIf config.mine.user.fish.enable {
+    xdg.configFile."fish/themes/Catppuccin Mocha.theme".source = ./catppuccin-mocha.theme;
     programs.fish = {
       enable = true;
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
         set -x NIX_PATH nixpkgs=channel:nixos-unstable
+        fish_config theme choose "Catppuccin Mocha"
       '';
       loginShellInit = ''
         set fish_greeting # Disable greeting
       '';
     };
-    catppuccin.fish.enable = true;
+    catppuccin.fish.enable = false;
     stylix.targets.fish.enable = false;
   };
 }
