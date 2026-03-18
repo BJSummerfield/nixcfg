bb65a49a0f265e89a1565ee80a1e6ade1c1f0cc2
BJSummerfield <brianjsummerfield@gmail.com>
2026-03-17 23:10:09 -0500

    comment out immich

--- hosts/redtruck/default.nix
+++ hosts/redtruck/default.nix
@@ -30,7 +30,6 @@
       fish.enable = true;
       _1password.enable = true;
       avahi.enable = true;
-      # immich-ml.enable = true;
       makemkv.enable = true;
       nas = {
         shares.media.enable = true;
