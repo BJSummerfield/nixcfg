5b66e3f9b721fe7f3b2bc1fbf88d50afb37ec6df
BJSummerfield <brianjsummerfield@gmail.com>
2026-03-20 17:27:59 -0500

    remove extra config

--- modules/immich-server/nixos.nix
+++ modules/immich-server/nixos.nix
@@ -134,8 +134,6 @@ in
 
         services.immich = {
           enable = true;
-          port = 2283;
-          host = "0.0.0.0";
           openFirewall = false;
           mediaLocation = "/var/lib/immich";
           accelerationDevices = [ "/dev/dri/renderD128" ];
