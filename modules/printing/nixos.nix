194655b7b6a90d14f75d23a4c34573fc8ebf9584
BJSummerfield <brianjsummerfield@gmail.com>
2026-03-22 17:12:21 -0500

    update printing

--- modules/printing/nixos.nix
+++ modules/printing/nixos.nix
@@ -3,6 +3,12 @@
   options.mine.system.printing.enable = lib.mkEnableOption "Enable printing service";
 
   config = lib.mkIf config.mine.system.printing.enable {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      drivers = [ ];
+    };
+
+    services.avahi.publish.enable = true;
+    services.avahi.publish.userServices = true;
   };
 }
