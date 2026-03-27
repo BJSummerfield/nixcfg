4a6661487ee82155e06b00b650bde4b77255da13
BJSummerfield <brianjsummerfield@gmail.com>
2026-03-27 09:35:49 -0500

    trying out the kde polkit and update xwayland

--- modules/polkit_gnome/home.nix
+++ modules/polkit_gnome/home.nix
@@ -20,7 +20,24 @@
     #     };
     #   };
     # };
-    services.polkit-gnome.enable = true;
+
+
+    systemd.user.services.polkit-kde-agent = {
+      description = "PolicyKit KDE Authentication Agent";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+    # services.polkit-gnome.enable = true;
+
+    # security.polkit.enable = true;
     # home.packages = with pkgs; [
     #   polkit_gnome
     # ];
