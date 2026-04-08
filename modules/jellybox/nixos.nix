c30340502271e9cf12ad40b3b9039786bb2137d0
BJSummerfield <brianjsummerfield@gmail.com>
2026-04-08 17:49:01 -0500

    update jellybox

--- modules/jellybox/nixos.nix
+++ modules/jellybox/nixos.nix
@@ -7,9 +7,15 @@
     boot.kernelParams = [ "jellybox" ];
     services.getty.autologinUser = "jellyuser";
 
+
     specialisation.desktop.configuration = {
       boot.kernelParams = lib.mkForce [ ];
       services.getty.autologinUser = lib.mkForce null;
+
+      # this is to stop laptop power events with the screen
+      services.logind.lidSwitch = lib.mkForce "suspend";
+      services.logind.lidSwitchDocked = lib.mkForce "ignore";
+      services.logind.lidSwitchExternalPower = lib.mkForce "ignore";
     };
 
     mine.system.gamescope.enable = true;
