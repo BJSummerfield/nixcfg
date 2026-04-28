eced88293343d0340ae5d393da68efe9aaa94731
BJSummerfield <brianjsummerfield@gmail.com>
2026-04-28 17:32:20 -0500

    update modules to allow tailscale and lan ssh

--- modules/openssh/nixos.nix
+++ modules/openssh/nixos.nix
@@ -1,19 +1,37 @@
 { lib, config, ... }:
+let
+  cfg = config.mine.system.openssh;
+  externalInterface = config.mine.system.externalInterface;
+in
 {
   options.mine.system.openssh = {
-    enable = lib.mkEnableOption "Enable open SSH";
+    enable = lib.mkEnableOption "OpenSSH server";
+
+    openOnExternalInterface = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Whether to allow SSH on the host's external (LAN/WAN) interface
+        defined in mine.system.externalInterface.
+        Enable for stationary machines on a trusted LAN.
+        Leave false for laptops and VPSes — those rely on tailnet access only.
+      '';
+    };
   };
 
-  config = lib.mkIf config.mine.system.openssh.enable {
+  config = lib.mkIf cfg.enable {
     services.openssh = {
       enable = true;
+      openFirewall = false;
       settings = {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
       };
     };
 
-    # ssh is disabled on boot, use systemctl to turn it on when needed with 'start'
-    systemd.services.sshd.wantedBy = lib.mkForce [ ];
+    networking.firewall.interfaces = lib.mkIf cfg.openOnExternalInterface {
+      ${externalInterface}.allowedTCPPorts = [ 22 ];
+    };
   };
 }
--- modules/tailscale/nixos.nix
+++ modules/tailscale/nixos.nix
@@ -1,7 +1,32 @@
-{ config, lib, ... }:
+{ lib, config, ... }:
+let
+  cfg = config.mine.system.tailscale;
+in
 {
-  options.mine.system.tailscale.enable = lib.mkEnableOption "Enable Tailscale";
-  config = lib.mkIf config.mine.system.tailscale.enable {
-    services.tailscale.enable = true;
+  options.mine.system.tailscale =
+    {
+      enable = lib.mkEnableOption "Tailscale";
+
+      ssh = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Whether to allow SSH connections over the tailnet interface.
+          Requires mine.system.openssh.enable for an sshd to actually be listening.
+        '';
+      };
+
+    };
+
+  config = lib.mkIf cfg.enable {
+    services.tailscale = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    mine.system.openssh.enable = lib.mkIf cfg.ssh true;
+    networking.firewall.interfaces = lib.mkIf cfg.ssh {
+      "tailscale0".allowedTCPPorts = [ 22 ];
+    };
   };
 }
