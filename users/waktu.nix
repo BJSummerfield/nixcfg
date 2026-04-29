360e593e8d13c14edba460b1cc5e01207bc56305
BJSummerfield <brianjsummerfield@gmail.com>
2026-04-28 22:35:42 -0500

    update for keys

--- users/waktu.nix
+++ users/waktu.nix
@@ -5,9 +5,10 @@
     isSuperUser = true;
 
     initialHashedPassword = "$y$j9T$IoChbWGYRh.rKfmm0G86X0$bYgsWqDRkvX.EBzJTX.Z0RsTlwspADpvEF3QErNyCMC";
-    sshKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtTarFZkhNoHtu39C6eCRaS84jb6SPoY92gn64Q2D3O"
-    ];
+    sshKeys = {
+      onepassword = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtTarFZkhNoHtu39C6eCRaS84jb6SPoY92gn64Q2D3O";
+      t495 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgkxPsobIniuj+jWkUQCS2nCRfOAOaJdIXqExtNV7QD waktu@t495";
+    };
     nasAccess = {
       media = "rw";
       homes = "ro";
@@ -19,7 +20,7 @@
           user = {
             name = "BJSummerfield";
             email = "brianjsummerfield@gmail.com";
-            signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2G3biYuL3iFvhAXYNuVzvRpAQMmFFLek3KFZV4PfDu";
+            user.signingkey = "/home/waktu/.ssh/id_ed25519.pub";
           };
           gpg.format = "ssh";
           commit.gpgSign = true;
