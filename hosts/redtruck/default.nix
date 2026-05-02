ddb726c3667bf3b766a33fe63ca8b22441daf32c
BJSummerfield <brianjsummerfield@gmail.com>
2026-05-02 13:30:40 -0500

    test server

--- hosts/redtruck/default.nix
+++ hosts/redtruck/default.nix
@@ -42,7 +42,7 @@
       steam.enable = true;
       tailscale.enable = true;
       teamspeak-client.enable = true;
-      # tuigreet.enable = true;
+      terraria-server.enable = true;
     };
   };
   home-manager.users = {
