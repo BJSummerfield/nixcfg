c160ebb4869511bf161af8e70086282b20b97c7a
BJSummerfield <brianjsummerfield@gmail.com>
2026-05-03 10:45:27 -0500

    update jellybox

--- modules/jellybox/nixos.nix
+++ modules/jellybox/nixos.nix
@@ -3,14 +3,14 @@
   options.mine.system.jellybox.enable = lib.mkEnableOption "Jellybox system dependencies";
   config = lib.mkIf config.mine.system.jellybox.enable {
 
-    boot.loader.timeout = 5;
-    boot.kernelParams = [ "jellybox" ];
-    services.getty.autologinUser = "jellyuser";
+    # boot.loader.timeout = 5;
+    # boot.kernelParams = [ "jellybox" ];
+    # services.getty.autologinUser = "jellyuser";
 
 
-    specialisation.desktop.configuration = {
-      boot.kernelParams = lib.mkForce [ ];
-      services.getty.autologinUser = lib.mkForce null;
+    specialisation.jellybox.configuration = {
+      # boot.kernelParams = lib.mkForce [ ];
+      # services.getty.autologinUser = lib.mkForce null;
 
       # this is to stop laptop power events with the screen
       services.logind.lidSwitch = lib.mkForce "suspend";
