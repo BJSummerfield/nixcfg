c30f17cca5749e9318b90b56781ebad065058c3b
BJSummerfield <brianjsummerfield@gmail.com>
2026-05-27 18:08:35 -0500

    fix stylix

--- modules/lazygit/home.nix
+++ modules/lazygit/home.nix
@@ -1,16 +1,16 @@
 { lib, config, options, ... }:
+let
+  inherit (lib) mkIf mkEnableOption optionalAttrs;
+in
 {
-  options.mine.user.lazygit.enable = lib.mkEnableOption "User Lazygit";
-  config = lib.mkIf config.mine.user.lazygit.enable (lib.mkMerge [
-    {
-      programs.lazygit = {
-        enable = true;
-        settings.gui.language = "en";
-      };
-      home.shellAliases.lg = "lazygit";
-    }
-    (lib.mkIf (options ? stylix) {
-      stylix.targets.lazygit.enable = true;
-    })
-  ]);
+  options.mine.user.lazygit.enable = mkEnableOption "User Lazygit";
+  config = mkIf config.mine.user.lazygit.enable ({
+    programs.lazygit = {
+      enable = true;
+      settings.gui.language = "en";
+    };
+    home.shellAliases.lg = "lazygit";
+  } // optionalAttrs (options ? stylix) {
+    stylix.targets.lazygit.enable = true;
+  });
 }
--- modules/swaylock/home.nix
+++ modules/swaylock/home.nix
@@ -1,29 +1,26 @@
 { pkgs, config, lib, options, ... }:
 let
-  inherit (lib) mkEnableOption mkIf mkMerge;
+  inherit (lib) mkEnableOption mkIf optionalAttrs;
   cfg = config.mine.user.swaylock;
 in
 {
   options.mine.user.swaylock.enable = mkEnableOption "Enable swaylock config";
-  config = mkIf cfg.enable (mkMerge [
-    {
-      programs.swaylock = {
-        enable = true;
-        package = pkgs.swaylock-effects;
-        settings = {
-          screenshots = true;
-          clock = true;
-          indicator = true;
-          indicator-radius = 100;
-          indicator-thickness = 7;
-          effect-blur = "7x5";
-          effect-vignette = "0.5:0.5";
-          fade-in = 1;
-        };
+  config = mkIf cfg.enable ({
+    programs.swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        screenshots = true;
+        clock = true;
+        indicator = true;
+        indicator-radius = 100;
+        indicator-thickness = 7;
+        effect-blur = "7x5";
+        effect-vignette = "0.5:0.5";
+        fade-in = 1;
       };
-    }
-    (mkIf (options ? stylix) {
-      stylix.targets.swaylock.enable = true;
-    })
-  ]);
+    };
+  } // optionalAttrs (options ? stylix) {
+    stylix.targets.swaylock.enable = true;
+  });
 }
