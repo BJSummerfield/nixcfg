71cd4b16b53a7e7c812d272f8aa583d564627196
BJSummerfield <brianjsummerfield@gmail.com>
2026-07-09 19:34:09 -0500

    add large qwen

--- modules/local-llm/nixos.nix
+++ modules/local-llm/nixos.nix
@@ -8,13 +8,13 @@
 let
   cfg = config.mine.system.local-llm;
 
-  # qwenMtpName = "Qwen3.6-35B-A3B-MTP-GGUF";
-  # qwenMtpFile = "Qwen3.6-35B-A3B-UD-Q8_K_XL.gguf";
-  # qwenMtpModel = pkgs.fetchurl {
-  #   url = "https://huggingface.co/unsloth/${qwenMtpName}/resolve/main/${qwenMtpFile}";
-  #   hash = "sha256-cHpVqKQ5fs3kTeDEmdPmjBrR0kDR2mWCa0lJ0QQ/RFA=";
-  # };
-  # qwenMtpPath = "/var/lib/models/${qwenMtpFile}";
+  qwenMtpName = "Qwen3.6-35B-A3B-MTP-GGUF";
+  qwenMtpFile = "Qwen3.6-35B-A3B-UD-Q8_K_XL.gguf";
+  qwenMtpModel = pkgs.fetchurl {
+    url = "https://huggingface.co/unsloth/${qwenMtpName}/resolve/main/${qwenMtpFile}";
+    hash = "sha256-cHpVqKQ5fs3kTeDEmdPmjBrR0kDR2mWCa0lJ0QQ/RFA=";
+  };
+  qwenMtpPath = "/var/lib/models/${qwenMtpFile}";
 
   coderNextName = "Qwen3-Coder-Next-GGUF";
   coderNextFile = "Qwen3-Coder-Next-UD-Q3_K_XL.gguf";
@@ -70,25 +70,9 @@ in
         "/dev/kfd" = { hostPath = "/dev/kfd"; isReadOnly = false; };
         "/run/opengl-driver" = { hostPath = "/run/opengl-driver"; isReadOnly = true; };
         "/var/lib" = { hostPath = "/var/lib/local-llm"; isReadOnly = false; };
-        # "/var/lib/models/${qwenMtpFile}" = { hostPath = "${qwenMtpModel}"; isReadOnly = true; };
+        "/var/lib/models/${qwenMtpFile}" = { hostPath = "${qwenMtpModel}"; isReadOnly = true; };
         "/var/lib/models/${coderNextFile}" = { hostPath = "${coderNextModel}"; isReadOnly = true; };
       };
-      # "Qwen3.6-35B-A3B-MTP":
-      #   ttl: 3600
-      #   cmd: |
-      #     ${llamaServer}
-      #     --host 127.0.0.1 --port ''${PORT}
-      #     -m ${qwenMtpPath}
-      #     --alias Qwen3.6-35B-A3B-MTP
-      #     --temp 0.6
-      #     --top-p 0.95
-      #     --top-k 20
-      #     --min-p 0.0
-      #     --chat-template-kwargs '{"preserve_thinking":true}'
-      #     --ctx-size 131072
-      #     --spec-type draft-mtp --spec-draft-n-max 2 
-      #     --n-gpu-layers 99
-      #     --n-cpu-moe 27
 
       config = { config, pkgs, lib, ... }:
         let
@@ -97,6 +81,22 @@ in
             healthCheckTimeout: 300
             logLevel: info
             models:
+            "Qwen3.6-35B-A3B-MTP":
+              ttl: 3600
+              cmd: |
+                ${llamaServer}
+                --host 127.0.0.1 --port ''${PORT}
+                -m ${qwenMtpPath}
+                --alias Qwen3.6-35B-A3B-MTP
+                --temp 0.6
+                --top-p 0.95
+                --top-k 20
+                --min-p 0.0
+                --chat-template-kwargs '{"preserve_thinking":true}'
+                --ctx-size 131072
+                --spec-type draft-mtp --spec-draft-n-max 2 
+                --n-gpu-layers 99
+                --n-cpu-moe 27
 
               "Qwen3-Coder-Next":
                 ttl: 3600
