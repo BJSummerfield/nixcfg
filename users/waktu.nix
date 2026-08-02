24538e71acd8a59fbb499f9d876c9e03bbe65e67
BJSummerfield <brianjsummerfield@gmail.com>
2026-08-02 13:38:59 -0500

    update t495 ssh key

--- users/waktu.nix
+++ users/waktu.nix
@@ -15,7 +15,7 @@
       hashedPasswordFile = config.sops.secrets."waktu/password_hash".path;
       sshKeys = {
         onepassword = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtTarFZkhNoHtu39C6eCRaS84jb6SPoY92gn64Q2D3O";
-        t495 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiPGYO7ptuhbBGRgJ7QDnPgElAE3osPpNnaDO7LzUAT waktu@t495";
+        t495 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB7J5ZniIdU5XnuPLC4xB4bTuQpHMSWBWqfJ1NlJrewj waktu@t495";
         redtruck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG6p+QgFfhPs3W2FWXLhtdn0d1ZUuXllOdbrmjglMr0K waktu@redtruck";
         mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIJvp5+k7aS98u1vw7Vg4qd8M4kTg8SrKT+q5rHEtcHz brian@mac.local";
       };
