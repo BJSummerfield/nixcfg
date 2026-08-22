e1eca64eb9da7487fb478e13a8831417be13f38c
BJSummerfield <brianjsummerfield@gmail.com>
2026-08-22 14:16:33 -0500

    feat(photoform): pin the app's shipped contract and install its config

--- flake.nix
+++ flake.nix
@@ -86,6 +86,7 @@
         {
           encode_queue = pkgs.callPackage ./modules/encode_queue/package.nix { };
           caddy-l4 = pkgs.callPackage ./modules/caddy/package.nix { };
+          photoform = pkgs.callPackage ./modules/photoform/package.nix { };
         }
       );
 
