553b3fea97383ba0ace77a21cce2de6fae0c7caf
BJSummerfield <brianjsummerfield@gmail.com>
2026-08-21 20:58:52 -0500

    wip(photoform): real source hash

--- modules/photoform/package.nix
+++ modules/photoform/package.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage {
     owner = "BJSummerfield";
     repo = "Sheet-Automation-FF";
     rev = "306b3a8f25f2d48c2953af7879f2f63090c83a0d";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-goPjgkUrh/fGjb//G4JJlV/FjgywGeenhDqv35nPU9k=";
     # Routes the fetch through api.github.com with a netrc built from
     # NIX_GITHUB_PRIVATE_USERNAME/PASSWORD in the nix-daemon environment.
     private = true;
